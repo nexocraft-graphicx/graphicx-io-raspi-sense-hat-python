e9677f7b50736491a27c58db0c275429171e4892
sludwig <1314803+iosven@users.noreply.github.com>
2021-04-02 01:09:27 +0200

    use numpy

--- ledmatrix/thex.py
+++ ledmatrix/thex.py
@@ -1,75 +1,59 @@
 from sense_hat import SenseHat
 
+import numpy as np
+
+npcolor2 = np.array([0, 208, 193])
+npcolor3 = np.array([130, 141, 152])
+npcolor5 = np.array([59, 71, 82])
+npcolorred = np.array([130, 41, 52])
+npcoloryellow = np.array([130, 141, 52])
+
+nplit = npcolor2
+npdimmed = npcolor3
+npdark = npcolor5
+npred = npcolorred
+npyellow = npcoloryellow
+
+lit = nplit.asarray
+dimmed = npcolor3.asarray
+dark = npdark.asarray
+red = npred.asarray
+yellow = npyellow.asarray
+
 
 def the_x_lit(sense):
-    O = [59, 71, 82]  # dark
-    X = [0, 208, 193]  # lit
-    x = [
-        O, O, O, O, O, O, O, O,
-        O, O, O, O, O, O, X, X,
-        O, X, X, O, O, X, X, O,
-        O, O, X, X, X, X, O, O,
-        O, O, O, X, X, O, O, O,
-        O, O, X, X, X, X, O, O,
-        O, X, X, O, O, X, X, O,
-        X, X, O, O, O, O, O, O
-    ]
-    sense.set_pixels(x)
+    internal_draw(sense, dark, lit)
 
 
 def the_x_dimmed(sense):
-    O = [59, 71, 82]  # dark
-    X = [130, 141, 152]  # dimmed
-    x = [
-        O, O, O, O, O, O, O, O,
-        O, O, O, O, O, O, X, X,
-        O, X, X, O, O, X, X, O,
-        O, O, X, X, X, X, O, O,
-        O, O, O, X, X, O, O, O,
-        O, O, X, X, X, X, O, O,
-        O, X, X, O, O, X, X, O,
-        X, X, O, O, O, O, O, O
-    ]
-    sense.set_pixels(x)
+    internal_draw(sense, dark, dimmed)
 
 
 def the_x_in_red(sense):
-    O = [59, 71, 82]  # dark
-    X = [130, 41, 52]  # red
-    x = [
-        O, O, O, O, O, O, O, O,
-        O, O, O, O, O, O, X, X,
-        O, X, X, O, O, X, X, O,
-        O, O, X, X, X, X, O, O,
-        O, O, O, X, X, O, O, O,
-        O, O, X, X, X, X, O, O,
-        O, X, X, O, O, X, X, O,
-        X, X, O, O, O, O, O, O
-    ]
-    sense.set_pixels(x)
+    internal_draw(sense, dark, red)
 
 
 def the_x_in_yellow(sense):
-    O = [59, 71, 82]  # dark
-    X = [130, 141, 52]  # yellow
+    internal_draw(sense, dark, yellow)
+
+
+def internal_draw(sense, o, X):
     x = [
-        O, O, O, O, O, O, O, O,
-        O, O, O, O, O, O, X, X,
-        O, X, X, O, O, X, X, O,
-        O, O, X, X, X, X, O, O,
-        O, O, O, X, X, O, O, O,
-        O, O, X, X, X, X, O, O,
-        O, X, X, O, O, X, X, O,
-        X, X, O, O, O, O, O, O
+        o, o, o, o, o, o, o, o,
+        o, o, o, o, o, o, X, X,
+        o, X, X, o, o, X, X, o,
+        o, o, X, X, X, X, o, o,
+        o, o, o, X, X, o, o, o,
+        o, o, X, X, X, X, o, o,
+        o, X, X, o, o, X, X, o,
+        X, X, o, o, o, o, o, o
     ]
     sense.set_pixels(x)
 
 
 def the_x_wiped(sense):
-    O = [59, 71, 82]  # dark
-    sense.clear(O)
+    sense.clear(dark)
 
 
 def the_x_off(sense):
-    O = [0, 0, 0]  # off
-    sense.clear(O)
+    sense.clear([0, 0, 0])
