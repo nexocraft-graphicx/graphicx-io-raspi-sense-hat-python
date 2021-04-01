c1af21596de01d7cbf20064341e971cfe47e141f
sludwig <1314803+iosven@users.noreply.github.com>
2021-04-02 00:38:01 +0200

    coloring

--- ledmatrix/thex.py
+++ ledmatrix/thex.py
@@ -1,8 +1,9 @@
 from sense_hat import SenseHat
 
-def draw_the_x(sense):
-    O = [12, 12, 12]  # Dark
-    X = [100, 100, 100]  # Light
+
+def the_x_lit(sense):
+    O = [59, 71, 82]  # dark
+    X = [0, 208, 193]  # lit
     x = [
         O, O, O, O, O, O, O, O,
         O, O, O, O, O, O, X, X,
@@ -16,6 +17,59 @@ def draw_the_x(sense):
     sense.set_pixels(x)
 
 
-def wipe_the_x(sense):
-    O = [12, 12, 12]  # Dark
+def the_x_dimmed(sense):
+    O = [59, 71, 82]  # dark
+    X = [130, 141, 152]  # dimmed
+    x = [
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, X, X,
+        O, X, X, O, O, X, X, O,
+        O, O, X, X, X, X, O, O,
+        O, O, O, X, X, O, O, O,
+        O, O, X, X, X, X, O, O,
+        O, X, X, O, O, X, X, O,
+        X, X, O, O, O, O, O, O
+    ]
+    sense.set_pixels(x)
+
+
+def the_x_in_red(sense):
+    O = [59, 71, 82]  # dark
+    X = [130, 41, 52]  # red
+    x = [
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, X, X,
+        O, X, X, O, O, X, X, O,
+        O, O, X, X, X, X, O, O,
+        O, O, O, X, X, O, O, O,
+        O, O, X, X, X, X, O, O,
+        O, X, X, O, O, X, X, O,
+        X, X, O, O, O, O, O, O
+    ]
+    sense.set_pixels(x)
+
+
+def the_x_in_yellow(sense):
+    O = [59, 71, 82]  # dark
+    X = [130, 141, 52]  # yellow
+    x = [
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, X, X,
+        O, X, X, O, O, X, X, O,
+        O, O, X, X, X, X, O, O,
+        O, O, O, X, X, O, O, O,
+        O, O, X, X, X, X, O, O,
+        O, X, X, O, O, X, X, O,
+        X, X, O, O, O, O, O, O
+    ]
+    sense.set_pixels(x)
+
+
+def the_x_wiped(sense):
+    O = [59, 71, 82]  # dark
+    sense.clear(O)
+
+
+def the_x_off(sense):
+    O = [0, 0, 0]  # off
     sense.clear(O)
