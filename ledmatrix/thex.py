c3d6a1d08e602404b79c287b335077b19712de0d
sludwig <1314803+iosven@users.noreply.github.com>
2021-04-02 02:15:41 +0200

    better colors

--- ledmatrix/thex.py
+++ ledmatrix/thex.py
@@ -10,10 +10,10 @@ npcolorred = np.array([130, 41, 52])
 npcoloryellow = np.array([130, 141, 52])
 
 nplit = (npcolor2 - 7).clip(min=0)
-npdimmed = (npcolor3 - 10).clip(min=10)
+npdimmed = (npcolor3 - 20).clip(min=10)
 npvague = (npcolor4 - 7).clip(min=10)
 #npdark = np.subtract(npcolor5, np.array([20, 24, 30])).clip(min=10)
-npdark = (npcolor5 - 7).clip(min=10)
+npdark = (npcolor5 - 20).clip(min=10)
 npred = npcolorred
 npyellow = npcoloryellow
 
