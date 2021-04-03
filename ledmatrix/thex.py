b2490dd486805115958e35d890347c4bd1bbb8cc
sludwig <1314803+iosven@users.noreply.github.com>
2021-04-03 21:12:00 +0200

    fancy dark

--- ledmatrix/thex.py
+++ ledmatrix/thex.py
@@ -17,7 +17,7 @@ npdimmed = npcolor3
 npvague = npcolor4
 # npdark = np.subtract(npcolor5, np.array([20, 24, 30])).clip(min=10)
 # npdark = (npcolor5 - 20).clip(min=10)
-npdark = np.array([16, 8, 12])
+npdark = np.array([8, 4, 8])
 npred = npcolorred
 npyellow = npcoloryellow
 
